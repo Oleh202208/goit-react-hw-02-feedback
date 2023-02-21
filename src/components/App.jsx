b78ee9e32b26a0ab3e663dbe240217e3884a4700
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2023-02-21 12:58:44 +0100

    start go

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, {Component} from 'react';
+
+class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+  
+  render() {
+    return (
+    <div className="Section">
+      <h2 className="title">Please leave feedback</h2>
+    <button className="Btn" type="button">Good</button>
+    <button className="Btn" type="button">Neutral</button>
+    <button className="Btn" type="button">Bad</button>
+<h2 className="title">Statistics</h2>
+<ul className="List">
+  <li className="item">Good</li>
+  <li className="item">Neutral</li>
+  <li className="item">Bad</li>
+  <li className="item">Total</li>
+  <li className="item">Positive feedback</li>
+</ul>
+</div>
+)};
+    };
+
+    export {App};
