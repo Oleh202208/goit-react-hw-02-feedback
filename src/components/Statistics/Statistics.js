68e9288427d4f184a15000b191642980dcb703d4
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2023-02-25 16:00:00 +0100

    import

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,4 +1,4 @@
-import PropTypes from 'react';
+import PropTypes from 'prop-types';
 import { List } from './Statistics.styled';
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
